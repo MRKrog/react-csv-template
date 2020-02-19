281a7fca7f17aecfb55607c214a18aba73afb802
Michael Krog <michaelryankrog@gmail.com>
2020-02-19 14:17:30 -0600

    update to parsing csv

--- src/views/Home/Upload.js
+++ src/views/Home/Upload.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
+import Papa from 'papaparse';
 
 class Upload extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedFile: null,
-      loaded: 0
-    }
+      csvfile: undefined
+    };
   }
 
-  onClickHandler = () => {
-    const data = new FormData()
-    data.append('file', this.state.selectedFile)
+  handleChange = event => {
+    this.setState({
+      csvfile: event.target.files[0]
+    });
+  };
+
+  importCSV = () => {
+    const { csvfile } = this.state;
+    Papa.parse(csvfile, {
+      complete: this.updateData,
+      header: true
+    });
+  };
+
+  updateData = (result) => {
+    var data = result.data;
     console.log(data);
   }
 
-  onChangeHandler=event=>{
-    this.setState({
-      selectedFile: event.target.files[0],
-      loaded: 0,
-    })
+  onClickHandler = async () => {
+
+    let options = {
+      method: 'POST',
+      body: formData
+    }
+
+    try {
+      const response = await fetch("http://localhost:3001/fileupload", options)
+      if(!response.ok) { throw new Error(`Fetch Call Cannot Be Made`)}
+      await response.json();
+    } catch (error) {
+      console.log('error', error.message);
+    }
   }
 
   render() {
+   console.log("Render File data: ",this.state.csvfile);
+
     return (
       <div className="container">
         <label>Upload Your File </label>
-        <input type="file" className="form-control" multiple onChange={this.onChangeHandler}/>
-        <button type="button" onClick={this.onClickHandler}>Upload</button>
+        <input type="file" className="form-control" onChange={this.handleChange}/>
+        <button type="button" onClick={this.importCSV}>Upload</button>
       </div>
     );
   }
